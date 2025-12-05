b1dcc649ef17da0d78a8e5cfec5f5ff6f39adecb
Domantas111 <domantas.gtx@gmail.com>
2025-12-05 16:53:28 +0200

    should be fine. runOpMode is main function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AutoOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AutoOp.java
@@ -1,4 +1,27 @@
 package org.firstinspires.ftc.teamcode.opmodes.auto;
 
-public class AutoOp {
+import com.bylazar.telemetry.JoinedTelemetry;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.seattlesolvers.solverslib.gamepad.GamepadEx;
+
+import org.firstinspires.ftc.teamcode.config.commands.ShootCommand;
+import org.firstinspires.ftc.teamcode.config.subsystems.Chassis;
+import org.firstinspires.ftc.teamcode.config.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.config.subsystems.Shooter;
+
+@Autonomous
+public class AutoOp extends LinearOpMode {
+    private Intake intake;
+    private Shooter shooter;
+    private Chassis chassis;
+    private ShootCommand shootCommand;
+    @Override
+    public void runOpMode() {
+        telemetry = new JoinedTelemetry(PanelsTelemetry.INSTANCE.getFtcTelemetry(), telemetry);
+        intake = new Intake(hardwareMap, telemetry);
+        shooter = new Shooter(hardwareMap, telemetry);
+        chassis = new Chassis(hardwareMap, telemetry);
+    }
 }
