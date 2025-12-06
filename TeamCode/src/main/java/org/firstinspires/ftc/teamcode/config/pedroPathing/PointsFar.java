7f197ab94427d61228ed65ec2c093a2ffe10ad1c
Pumkinhead <moliuggalvis@gmail.com>
2025-12-06 11:52:56 +0200

    Added exit launch auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/pedroPathing/PointsFar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/pedroPathing/PointsFar.java
@@ -6,11 +6,12 @@ public class PointsFar {
     public static Pose getGoalPose(double x, double y){
         return new Pose(x, y, Math.toRadians(90) -Math.atan2(0-x, 144-y));
     }
-    public static final Pose START_POS = new Pose(60, 8.2, Math.toRadians(90));
-    public static final Pose SHOOT_POS = new Pose(60, 40, Math.toRadians(135));
+    public static final Pose START_POS = new Pose(55, 8.2, Math.toRadians(90));
+    public static final Pose EXIT_POS = new Pose(55, 35, Math.toRadians(90));
+    public static final Pose SHOOT_POS = new Pose(55, 40, Math.toRadians(135));
     public static final Pose SHOOT_POS1 = new Pose(0, 8.2, Math.toRadians(90));
     public static final Pose SHOOT_POS2 = new Pose(0, 40, Math.toRadians(90));
-    public static final Pose SHOOT_POS3 = new Pose(60, 120, Math.toRadians(90));
+    public static final Pose SHOOT_POS3 = new Pose(55, 120, Math.toRadians(90));
 
     //INTAKE_1
     public static final Pose PREPARE_TO_INTAKE_POS_1 = new Pose(36, 84, Math.toRadians(180));
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AutoDriveForward.java
@@ -0,0 +1,122 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto;
+
+import static org.firstinspires.ftc.teamcode.config.pedroPathing.PointsFar.EXIT_POS;
+import static org.firstinspires.ftc.teamcode.config.pedroPathing.PointsFar.START_POS;
+
+import com.bylazar.telemetry.JoinedTelemetry;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.seattlesolvers.solverslib.command.Command;
+
+import org.firstinspires.ftc.teamcode.config.commands.ShootCommand;
+import org.firstinspires.ftc.teamcode.config.pedroPathing.Constants;
+import org.firstinspires.ftc.teamcode.config.subsystems.Chassis;
+import org.firstinspires.ftc.teamcode.config.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.config.subsystems.Shooter;
+
+import java.util.ArrayList;
+
+@Autonomous(name = "Auto exit launch", group = "!")
+public class AutoDriveForward extends OpMode {
+    private class PathHolder extends StageHandler{
+        private PathChain path;
+        public PathHolder(PathChain path){
+            this.path = path;
+        }
+        @Override
+        public void start() {
+            f.followPath(path, false);
+        }
+        @Override
+        public boolean isBusy() {
+            return f.isBusy();
+        }
+    }
+    private class CommandHolder extends StageHandler {
+        private Command command;
+        public CommandHolder(Command command){
+            this.command = command;
+        }
+        @Override
+        public void start() {
+            command.schedule();
+        }
+
+        @Override
+        public boolean isBusy() {
+            return !command.isFinished();
+        }
+    }
+    private abstract class StageHandler {
+        public abstract void start();
+        public abstract boolean isBusy();
+    }
+
+
+
+
+
+
+
+    private Intake intake;
+    private Shooter shooter;
+    private Chassis chassis;
+    private ShootCommand shootCommand;
+    private Follower f;
+
+    private StageHandler[] stages;
+    int stageInd = -1;
+    public PathChain getPath(Pose pos1, Pose pos2){
+        return f.pathBuilder()
+                .addPath(new BezierLine(pos1, pos2))
+                .setConstantHeadingInterpolation(pos2.getHeading())
+                .build();
+    }
+    public void buildCommands(){
+        ArrayList<StageHandler> stagesList = new ArrayList<>();
+
+        stagesList.add(new PathHolder(getPath(START_POS, EXIT_POS)));
+
+        stages = new StageHandler[stagesList.size()];
+        stages = stagesList.toArray(stages);
+    }
+    @Override
+    public void init() {
+        telemetry = new JoinedTelemetry(PanelsTelemetry.INSTANCE.getFtcTelemetry(), telemetry);
+        intake = new Intake(hardwareMap, telemetry);
+        shooter = new Shooter(hardwareMap, telemetry);
+        chassis = new Chassis(hardwareMap, telemetry);
+        shootCommand = new ShootCommand(intake, shooter);
+
+        f = Constants.createFollower(hardwareMap);
+        f.setStartingPose(START_POS);
+
+        buildCommands();
+
+        telemetry.addData("Stages found: ", stages.length);
+        telemetry.update();
+    }
+
+    public void autoUpdate() {
+        telemetry.addData("Running loop with stage ", stageInd);
+        if(stageInd >= stages.length || (stageInd > 0 && stages[stageInd].isBusy())) return;
+        stageInd++;
+        if(stageInd >= stages.length) return;
+        stages[stageInd].start();
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addLine("Running loop");
+
+        f.update();
+        autoUpdate();
+
+        telemetry.update();
+    }
+}
